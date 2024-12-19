4c1b0216e588e85b7fd4d5ef3247998bc34bd0a4
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-19 13:10:36 +0530

    Updated for mobile

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -13,10 +13,12 @@ export function ModeToggle() {
 	return (
 		<DropdownMenu modal={false}>
 			<DropdownMenuTrigger asChild>
-				<Button variant='outline' size='icon'>
-					<Sun className='size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-					<Moon className='absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-					<span className='sr-only'>Toggle theme</span>
+				<Button variant='outline' className='md:size-10'>
+					<span className='relative flex h-5 w-5 items-center justify-center'>
+						<Sun className='size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+						<Moon className='absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+					</span>
+					<span className='inline md:sr-only'>Toggle Theme</span>
 				</Button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent align='end'>
