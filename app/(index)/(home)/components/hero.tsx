e66bfb0415bf75fccddee30ebf0b43e20669cefe
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-19 15:04:39 +0530

    Updated CTA

--- app/(index)/(home)/components/hero.tsx
+++ app/(index)/(home)/components/hero.tsx
@@ -1,29 +1,36 @@
 'use client';
 
 import { Section } from '@/components/layout';
-import { A, H1, P } from '@/components/typography';
-import { Badge } from '@/components/ui/badge';
-import { Button } from '@/components/ui/button';
-import { ArrowRightIcon } from 'lucide-react';
+import { H1, P } from '@/components/typography';
+import AnimatedShinyText from '@/components/ui/animated-shiny-text';
+import { RainbowButton } from '@/components/ui/rainbow-button';
+import { ArrowRightIcon, FlaskConicalIcon } from 'lucide-react';
 
 export default function Hero() {
 	return (
 		<Section className='relative grid h-svh w-full place-items-center'>
 			<div className='space-y-16 text-center'>
 				<div className='space-y-8'>
-					<Badge>Early Access</Badge>
+					<div className='mx-auto w-fit rounded-full border border-border px-2 py-1 text-sm'>
+						<AnimatedShinyText className='inline-flex items-center justify-center gap-2 px-4 py-1'>
+							<span>
+								<FlaskConicalIcon className='size-4 text-neutral-600/70 dark:text-neutral-400/70' />
+							</span>
+							<hr className='mx-2 h-4 w-px shrink-0 bg-neutral-400/70 dark:bg-neutral-700/70' />
+							Early Access
+						</AnimatedShinyText>
+					</div>
 					<H1 variant='hero'>Your Invoicing, Simplified.</H1>
 					<P variant='lg' className='mx-auto max-w-md'>
 						Create, manage, and export professional invoices in seconds—anytime, anywhere.
 					</P>
 				</div>
 
-				<div className='mt-8 space-x-4 md:space-x-8'>
-					<Button>
-						<ArrowRightIcon />
+				<div className='mt-8 flex flex-col items-center gap-8'>
+					<RainbowButton className='w-fit'>
+						<ArrowRightIcon className='mr-2' />
 						Get Started for Free
-					</Button>
-					<A href='#'>Learn more</A>
+					</RainbowButton>
 				</div>
 			</div>
 
