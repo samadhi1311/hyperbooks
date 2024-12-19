5f4f5abcbf316e1e9040b135716605603238e108
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-19 22:28:57 +0530

    Removed dropdown and updated to toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,31 +1,27 @@
 'use client';
 
-import * as React from 'react';
-import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-
 import { Button } from '@/components/ui/button';
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
+import { Moon, Sun } from 'lucide-react';
 
 export function ModeToggle() {
-	const { setTheme } = useTheme();
+	const { theme, setTheme } = useTheme();
+
+	function handleToggle() {
+		if (theme === 'dark') {
+			setTheme('light');
+		} else {
+			setTheme('dark');
+		}
+	}
 
 	return (
-		<DropdownMenu modal={false}>
-			<DropdownMenuTrigger asChild>
-				<Button variant='outline' className='md:size-10'>
-					<span className='relative flex h-5 w-5 items-center justify-center'>
-						<Sun className='size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-						<Moon className='absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
-					</span>
-					<span className='inline md:sr-only'>Toggle Theme</span>
-				</Button>
-			</DropdownMenuTrigger>
-			<DropdownMenuContent align='end'>
-				<DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-				<DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
-			</DropdownMenuContent>
-		</DropdownMenu>
+		<Button variant='outline' className='md:size-10' onClick={handleToggle}>
+			<span className='relative flex h-5 w-5 items-center justify-center'>
+				<Sun className='size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+				<Moon className='absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+			</span>
+			<span className='inline md:sr-only'>Toggle Theme</span>
+		</Button>
 	);
 }
