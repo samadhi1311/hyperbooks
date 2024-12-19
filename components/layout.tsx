fbdfeeb12fb3374504405e4619f6d8935bc96379
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-19 11:53:55 +0530

    Added layouts component

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { cn } from '@/lib/utils';
+
+function Page({ children, className }: { children: React.ReactNode; className?: string }) {
+	return <main className={cn('max-w-screen-2xl mx-auto px-8 min-h-svh', className)}>{children}</main>;
+}
+
+function Section({ children, className, variant = 'normal' }: { children: React.ReactNode; className?: string; variant?: 'normal' | 'main' }) {
+	const variants = {
+		normal: 'py-16',
+		main: 'py-0',
+	}[variant];
+	return <section className={cn(variants, className)}>{children}</section>;
+}
+
+export { Page, Section };
