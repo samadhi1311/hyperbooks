9cd26d3e2451a2aeaefd166ec2e15fa30a000e91
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-21 01:16:55 +0530

    Added unified muted colors

--- app/(index)/(home)/components/cta.tsx
+++ app/(index)/(home)/components/cta.tsx
@@ -10,7 +10,7 @@ export default function CTA() {
 		<Section className='relative'>
 			<div className='flex flex-col items-center gap-4 py-8'>
 				<H2>Start Creating Beautiful Invoices Today!</H2>
-				<P className='max-w-2xl text-center text-neutral-600 dark:text-neutral-400'>
+				<P className='max-w-2xl text-center text-muted-foreground'>
 					Take the stress out of invoicing with our simple yet powerful tools. Sign up today and make your billing as polished as your business!
 				</P>
 				<Button variant='secondary' className='mt-4 flex items-center gap-2'>
@@ -18,7 +18,7 @@ export default function CTA() {
 					Sign up
 				</Button>
 			</div>
-			<div className='absolute bottom-0 -z-50 h-full w-full bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-foreground/10 via-transparent to-transparent dark:from-foreground/10 dark:via-transparent dark:to-transparent' />
+			<div className='absolute bottom-0 -z-50 h-full w-full bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-foreground/10 via-transparent to-transparent dark:from-foreground/5 dark:via-transparent dark:to-transparent' />
 		</Section>
 	);
 }
