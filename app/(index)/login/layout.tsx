d2ea069d2983ee7ab792077ec7ff799b58c676b3
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-21 01:15:41 +0530

    Added Login page layout

---
+++ app/(index)/login/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+	title: 'Login | hyperbooks.',
+	description: 'Log in to your account to access hyperbooks.',
+};
+
+export default function LoginLayout({ children }: { children: React.ReactNode }) {
+	return <>{children}</>;
+}
