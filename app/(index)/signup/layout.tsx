08601ba2ebeadc218543471277660ae1705414de
samadhi1311 <samadhilakmina.sl@gmail.com>
2024-12-21 01:15:27 +0530

    Added Sign up page layout

---
+++ app/(index)/signup/layout.tsx
@@ -0,0 +1,8 @@
+export const metadata = {
+	title: 'Sign up | hyperbooks.',
+	description: 'Sign up for an account and start invoicing.',
+};
+
+export default function SignUpLayout({ children }: { children: React.ReactNode }) {
+	return <>{children}</>;
+}
