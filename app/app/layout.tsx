74b6620658b5c08ef931f994f69c6ef76c36781d
nipunigamage <nipunikanishka255@gmail.com>
2025-01-26 21:57:35 +0530

    Added Protected route for App

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { AppSidebar } from '@/components/app-sidebar';
+import { ProtectedRoute } from '@/components/protected-route';
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from '@/components/ui/breadcrumb';
 import { Separator } from '@/components/ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
@@ -10,33 +11,34 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
 	const active = pathname.replace('/app', '');
 	const path = active.split('/');
-	console.log(path);
 	return (
-		<SidebarProvider>
-			<AppSidebar />
-			<SidebarInset>
-				<header className='flex h-16 shrink-0 items-center gap-2'>
-					<div className='flex items-center gap-2 px-4'>
-						<SidebarTrigger className='-ml-1' />
-						<Separator orientation='vertical' className='mr-2 h-4' />
-						<Breadcrumb>
-							<BreadcrumbList>
-								{path.map((item, index) => (
-									<span key={index} className={path[index] === '' ? 'hidden' : 'flex items-center justify-center gap-2'}>
-										<BreadcrumbItem>
-											<BreadcrumbLink href={index === 1 ? '/app' : `/app${path.slice(0, index + 1).join('/')}`} className='capitalize'>
-												{item}
-											</BreadcrumbLink>
-										</BreadcrumbItem>
-										<BreadcrumbSeparator className={index === path.length - 1 ? 'hidden' : 'hidden md:block'} style={path.length < 1 ? { opacity: 0 } : { opacity: 1 }} />
-									</span>
-								))}
-							</BreadcrumbList>
-						</Breadcrumb>
-					</div>
-				</header>
-				<div className='flex flex-1 flex-col gap-4 p-4 pt-0'>{children}</div>
-			</SidebarInset>
-		</SidebarProvider>
+		<ProtectedRoute>
+			<SidebarProvider>
+				<AppSidebar />
+				<SidebarInset>
+					<header className='flex h-16 shrink-0 items-center gap-2'>
+						<div className='flex items-center gap-2 px-4'>
+							<SidebarTrigger className='-ml-1' />
+							<Separator orientation='vertical' className='mr-2 h-4' />
+							<Breadcrumb>
+								<BreadcrumbList>
+									{path.map((item, index) => (
+										<span key={index} className={path[index] === '' ? 'hidden' : 'flex items-center justify-center gap-2'}>
+											<BreadcrumbItem>
+												<BreadcrumbLink href={index === 1 ? '/app' : `/app${path.slice(0, index + 1).join('/')}`} className='capitalize'>
+													{item}
+												</BreadcrumbLink>
+											</BreadcrumbItem>
+											<BreadcrumbSeparator className={index === path.length - 1 ? 'hidden' : 'hidden md:block'} style={path.length < 1 ? { opacity: 0 } : { opacity: 1 }} />
+										</span>
+									))}
+								</BreadcrumbList>
+							</Breadcrumb>
+						</div>
+					</header>
+					<div className='flex flex-1 flex-col gap-4 p-4 pt-0'>{children}</div>
+				</SidebarInset>
+			</SidebarProvider>
+		</ProtectedRoute>
 	);
 }
