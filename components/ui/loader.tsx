dbf1576323b5793f63a6874c607c162bfbc869ed
nipunigamage <nipunikanishka255@gmail.com>
2025-01-26 21:56:59 +0530

    Updated Loader component

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -2,8 +2,16 @@ import { Loader2Icon } from 'lucide-react';
 
 export default function Loader() {
 	return (
-		<div className='flex h-full w-full items-center justify-center'>
-			<Loader2Icon className='size-5 animate-spin' />
+		<div className='absolute z-50 mx-auto flex h-screen w-full items-center justify-center gap-2 bg-background'>
+			<div className='flex items-center gap-4 text-foreground/80'>
+				<span className='relative block'>
+					<img src='/logo.svg' width={18} height={18} alt='hyperbooks Logo' />
+					<span className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'>
+						<Loader2Icon strokeWidth={2} className='size-10 animate-spin' />
+					</span>
+				</span>
+				<h1 className='text-4xl font-medium tracking-tighter'>hyperbooks.</h1>
+			</div>
 		</div>
 	);
 }
