b5dc12e1994413ee8a75066679035821d4771e25
nipunigamage <nipunikanishka255@gmail.com>
2025-01-26 19:40:20 +0530

    Added animating between page navigations

--- components/ui/layout.tsx
+++ components/ui/layout.tsx
@@ -1,9 +1,18 @@
 'use client';
 
+import { motion } from 'motion/react';
 import { cn } from '@/lib/utils';
 
 function PageWrapper({ children, className }: { children: React.ReactNode; className?: string }) {
-	return <main className={cn('max-w-screen-2xl w-full mx-auto px-8 2xl:px-0 min-h-svh', className)}>{children}</main>;
+	return (
+		<motion.main
+			initial={{ opacity: 0, y: 25 }}
+			animate={{ opacity: 1, y: 0 }}
+			transition={{ duration: 1, ease: [0.215, 0.61, 0.355, 1] }}
+			className={cn('max-w-screen-2xl w-full mx-auto px-8 2xl:px-0 min-h-svh', className)}>
+			{children}
+		</motion.main>
+	);
 }
 
 function Section({ children, className, variant = 'normal', ref }: { children: React.ReactNode; className?: string; variant?: 'normal' | 'main'; ref?: React.Ref<HTMLDivElement> }) {
