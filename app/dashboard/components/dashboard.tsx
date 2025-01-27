7ce52007c7e3dc39560eb3875dcada10b1d9f80b
nipunigamage <nipunikanishka255@gmail.com>
2025-01-27 20:41:56 +0530

    Improved responsive layout

--- app/dashboard/components/dashboard.tsx
+++ app/dashboard/components/dashboard.tsx
@@ -18,13 +18,13 @@ export default function Dashboard() {
 	return (
 		<div className='flex min-h-screen w-full flex-col'>
 			<main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8'>
-				<div className='grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4'>
+				<div className='grid gap-4 md:grid-cols-2 md:gap-8 xl:grid-cols-4'>
 					<Greeting />
 					<Revenue />
 					<Invoices />
 					<Outstanding />
 				</div>
-				<div className='grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3'>
+				<div className='grid gap-4 md:gap-8 xl:grid-cols-2 2xl:grid-cols-3'>
 					<div className='xl:col-span-2' x-chunk='dashboard-01-chunk-4'>
 						<Chart />
 					</div>
