379610300ba923a95e3bd462792a18a223361297
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-01-27 14:03:38 +0530

    Added dynamic button for login

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { MenuIcon, User2Icon } from 'lucide-react';
+import { Loader2Icon, MenuIcon, SquareDashedMousePointerIcon, User2Icon } from 'lucide-react';
 import { ModeToggle } from './theme-toggle';
 import Link from 'next/link';
 import { A } from './ui/typography';
@@ -8,14 +8,54 @@ import { Button } from './ui/button';
 import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 import { Badge } from './ui/badge';
 import { motion } from 'motion/react';
+import { useAuth } from '@/hooks/use-auth';
 
 export default function Navigation() {
+	const hasAnimated = sessionStorage.getItem('navAnimated');
+
+	const handleAnimationComplete = () => {
+		sessionStorage.setItem('navAnimated', 'true');
+	};
+
+	const { user, authLoading } = useAuth();
+
+	const AuthButton = () => {
+		if (authLoading) {
+			return (
+				<Button variant='outline' className='flex items-center gap-3'>
+					<Loader2Icon className='animate-spin' />
+					Loading
+				</Button>
+			);
+		}
+		if (!authLoading && !user) {
+			return (
+				<Link href='/login'>
+					<Button variant='outline' className='flex items-center gap-3'>
+						<User2Icon />
+						Login
+					</Button>
+				</Link>
+			);
+		} else {
+			return (
+				<Link href='/app'>
+					<Button variant='outline' className='flex items-center gap-3'>
+						<SquareDashedMousePointerIcon className='scale-x-[-1]' />
+						Dashboard
+					</Button>
+				</Link>
+			);
+		}
+	};
+
 	return (
 		<motion.header
 			className='fixed top-0 z-50 w-full border-b border-border px-8 py-4 opacity-0 backdrop-blur-md'
 			initial={{ y: -100, opacity: 0 }}
 			animate={{ y: 0, opacity: 1 }}
-			transition={{ duration: 1, ease: [0.215, 0.61, 0.355, 1], delay: 1.5 }}>
+			transition={{ duration: 1, ease: [0.215, 0.61, 0.355, 1], delay: hasAnimated ? 0 : 1.5 }}
+			onAnimationComplete={handleAnimationComplete}>
 			<nav className='mx-auto flex max-w-screen-2xl items-center justify-between gap-4'>
 				<span>
 					<A href='/' className='flex items-center gap-2'>
@@ -32,12 +72,7 @@ export default function Navigation() {
 
 				<span className='hidden items-center gap-4 md:flex'>
 					<ModeToggle />
-					<Link href='/login'>
-						<Button variant='outline'>
-							<User2Icon className='size-5' />
-							Login
-						</Button>
-					</Link>
+					<AuthButton />
 				</span>
 
 				<span className='inline md:hidden'>
@@ -66,12 +101,7 @@ export default function Navigation() {
 
 							<SheetFooter>
 								<div className='flex w-full flex-col gap-8'>
-									<Link href='/login'>
-										<Button variant='outline' className='w-full'>
-											<User2Icon className='size-5' />
-											Login
-										</Button>
-									</Link>
+									<AuthButton />
 									<ModeToggle />
 								</div>
 							</SheetFooter>
