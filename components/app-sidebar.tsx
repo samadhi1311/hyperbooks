2a15b0388df4a27cfde595bd42a39c0c55b3e0e4
nipunigamage <nipunikanishka255@gmail.com>
2025-01-28 10:22:15 +0530

    Fixed updated links

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { LayoutListIcon, PlusCircleIcon, SwatchBookIcon } from 'lucide-react';
+import { LayoutListIcon, PlusCircleIcon, RocketIcon, SwatchBookIcon } from 'lucide-react';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
@@ -30,8 +30,16 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 					<SidebarGroupLabel>Invoices</SidebarGroupLabel>
 					<SidebarMenu>
 						<SidebarMenuItem>
+							<SidebarMenuButton className={pathname === '/dashboard' ? 'bg-sidebar-accent/50' : ''} asChild>
+								<Link href='/dashboard/invoices/create' className='flex h-full items-center gap-3'>
+									<RocketIcon />
+									Dashboard
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+						<SidebarMenuItem>
 							<SidebarMenuButton className={pathname === '/dashboard/invoices/create' ? 'bg-sidebar-accent/50' : ''} asChild>
-								<Link href='/dashboard/invoices/create' className='flex items-center gap-3'>
+								<Link href='/dashboard/invoices/create' className='flex h-full items-center gap-3'>
 									<PlusCircleIcon />
 									Create
 								</Link>
@@ -39,7 +47,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 						</SidebarMenuItem>
 						<SidebarMenuItem>
 							<SidebarMenuButton className={pathname === '/dashboard/invoices/templates' ? 'bg-sidebar-accent/50' : ''} asChild>
-								<Link href='/dashboard/invoices/templates' className='flex items-center gap-3'>
+								<Link href='/dashboard/invoices/templates' className='flex h-full items-center gap-3'>
 									<SwatchBookIcon />
 									Templates
 								</Link>
@@ -47,7 +55,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
 						</SidebarMenuItem>
 						<SidebarMenuItem>
 							<SidebarMenuButton className={pathname === '/dashboard/invoices/templates' ? 'bg-sidebar-accent/50' : ''} asChild>
-								<Link href='/dashboard/invoices/templates' className='flex items-center gap-3'>
+								<Link href='/dashboard/invoices/templates' className='flex h-full items-center gap-3'>
 									<LayoutListIcon />
 									History
 								</Link>
