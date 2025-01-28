e7d487c9ae0e0b0ac97284ba9f180a36a2d8fa30
nipunigamage <nipunikanishka255@gmail.com>
2025-01-28 14:12:24 +0530

    Updated breadcrumbs

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -10,8 +10,7 @@ import Link from 'next/link';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
 	const pathname = usePathname();
-	const active = pathname.replace('/dashboard', '');
-	const path = active.split('/');
+	const path = pathname.split('/');
 	return (
 		<ProtectedRoute>
 			<SidebarProvider>
@@ -27,7 +26,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 										<span key={index} className={path[index] === '' ? 'hidden' : 'flex items-center justify-center gap-2'}>
 											<BreadcrumbItem>
 												<BreadcrumbLink asChild>
-													<Link href={index === 1 ? '/dashboard' : `/dashboard${path.slice(0, index + 1).join('/')}`} className='capitalize'>
+													<Link href={`${path.slice(0, index + 1).join('/')}`} className='capitalize'>
 														{item}
 													</Link>
 												</BreadcrumbLink>
