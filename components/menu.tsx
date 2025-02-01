dcdb38dbff831bb73c628522de338b17141ac8e5
nipunigamage <nipunikanishka255@gmail.com>
2025-02-01 22:03:42 +0530

    Updated to accept profile data seperately

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,25 +1,31 @@
-import { CloudUploadIcon, DownloadIcon, FilePlus2Icon, Loader2Icon, MenuIcon, ViewIcon } from 'lucide-react';
+import { CloudUploadIcon, DownloadIcon, FilePlus2Icon, Loader2Icon, MenuIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from './ui/dropdown-menu';
-import Link from 'next/link';
 import { useInvoiceStore } from '@/store/use-invoice';
 import { Document, pdf } from '@react-pdf/renderer';
 import templates, { TemplateKey } from '@/templates';
 import { useTemplateStore } from '@/store/use-templates';
 import { useToast } from '@/hooks/use-toast';
 import { useFirestoreAdd } from '@/hooks/use-firestore';
-import { InvoiceData } from '@/lib/types';
+import { InvoiceData, ProfileData } from '@/lib/types';
+import { useProfileStore } from '@/store/use-profile';
 
 export default function Menu() {
 	const { invoiceData, resetInvoiceData } = useInvoiceStore();
 	const { selectedTemplate } = useTemplateStore();
+	const { profile } = useProfileStore();
 	const { addInvoice, loading, error } = useFirestoreAdd<Partial<InvoiceData>>();
 	const { toast } = useToast();
+
+	const invoicePayload = {
+		data: invoiceData,
+		profile: profile as ProfileData,
+	};
 
 	const handleExportPDF = async () => {
 		try {
 			const SelectedRenderer = templates[selectedTemplate as TemplateKey].render;
-			const pdfDoc = <Document>{SelectedRenderer(invoiceData)}</Document>;
+			const pdfDoc = <Document>{SelectedRenderer(invoicePayload)}</Document>;
 
 			const blob = await pdf(pdfDoc).toBlob();
 
@@ -39,7 +45,7 @@ export default function Menu() {
 			toast({
 				variant: 'destructive',
 				title: 'An error occurred.',
-				description: 'Couldn&apos;t export to a PDF. Please try again.',
+				description: error as string,
 			});
 			console.error('PDF Export Error:', error);
 		}
@@ -92,12 +98,6 @@ export default function Menu() {
 									Save to cloud
 								</>
 							)}
-						</DropdownMenuItem>
-						<DropdownMenuItem className='flex cursor-pointer'>
-							<Link href='create/preview' className='flex cursor-pointer items-center gap-3'>
-								<ViewIcon />
-								Preview
-							</Link>
 						</DropdownMenuItem>
 						<DropdownMenuItem className='flex cursor-pointer items-center gap-3' onClick={handleExportPDF}>
 							<DownloadIcon />
