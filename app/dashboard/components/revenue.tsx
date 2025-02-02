d655114f38fda1c6cdfbdcf1e53c60a1a48cfa19
nipunigamage <nipunikanishka255@gmail.com>
2025-02-02 20:07:17 +0530

    Added Revenue with animations

--- app/dashboard/components/revenue.tsx
+++ app/dashboard/components/revenue.tsx
@@ -1,7 +1,19 @@
+'use client';
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useProfileStore } from '@/store/use-profile';
 import { DollarSign } from 'lucide-react';
+import NumberFlow from '@number-flow/react';
+import { useEffect, useState } from 'react';
 
 export default function Revenue() {
+	const { profile } = useProfileStore();
+	const [income, setIncome] = useState(0);
+
+	useEffect(() => {
+		setIncome(profile?.totalIncome ?? 0);
+	}, [profile?.totalIncome]);
+
 	return (
 		<Card>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
@@ -9,8 +21,7 @@ export default function Revenue() {
 				<DollarSign className='h-4 w-4 text-muted-foreground' />
 			</CardHeader>
 			<CardContent>
-				<div className='text-2xl font-bold'>$45,231.89</div>
-				<p className='text-xs text-muted-foreground'>+20.1% from last month</p>
+				<NumberFlow className='text-2xl font-bold' format={{ notation: 'standard', style: 'currency', currency: 'LKR' }} value={income} />
 			</CardContent>
 		</Card>
 	);
