33f72b3215bff3c683d21af9e12744e2e548dabd
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-02 20:09:11 +0530

    Added Invoice count with animations

--- app/dashboard/components/invoices.tsx
+++ app/dashboard/components/invoices.tsx
@@ -1,16 +1,24 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { DollarSign } from 'lucide-react';
+import NumberFlow from '@number-flow/react';
+import { useProfileStore } from '@/store/use-profile';
+import { useEffect, useState } from 'react';
 
 export default function Invoices() {
+	const { profile } = useProfileStore();
+	const [count, setCount] = useState(0);
+
+	useEffect(() => {
+		setCount(profile?.totalInvoiceCount ?? 0);
+	}, [profile?.totalInvoiceCount]);
 	return (
 		<Card>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-				<CardTitle className='text-sm font-medium'>Total Revenue</CardTitle>
+				<CardTitle className='text-sm font-medium'>Total Issued</CardTitle>
 				<DollarSign className='h-4 w-4 text-muted-foreground' />
 			</CardHeader>
 			<CardContent>
-				<div className='text-2xl font-bold'>$45,231.89</div>
-				<p className='text-xs text-muted-foreground'>+20.1% from last month</p>
+				<NumberFlow className='text-2xl font-bold' value={count} />
 			</CardContent>
 		</Card>
 	);
