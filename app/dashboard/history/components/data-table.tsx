5a57c83b075dd769de0f17785561b261fc333139
nipunigamage <nipunikanishka255@gmail.com>
2025-02-03 13:54:59 +0530

    Updated table cell alignment to vertical top

--- app/dashboard/history/components/data-table.tsx
+++ app/dashboard/history/components/data-table.tsx
@@ -35,7 +35,9 @@ export function DataTable<TData, TValue>({ columns, data }: DataTableProps<TData
 							table.getRowModel().rows.map((row) => (
 								<TableRow key={row.id} data-state={row.getIsSelected() && 'selected'}>
 									{row.getVisibleCells().map((cell) => (
-										<TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+										<TableCell key={cell.id} className='align-top'>
+											{flexRender(cell.column.columnDef.cell, cell.getContext())}
+										</TableCell>
 									))}
 								</TableRow>
 							))
