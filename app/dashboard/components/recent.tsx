7d426f9dc052eaf1dfd6af9d80daaa37691954d3
nipunigamage <nipunikanishka255@gmail.com>
2025-02-04 16:03:18 +0530

    Added 10 most recent invoices

--- app/dashboard/components/recent.tsx
+++ app/dashboard/components/recent.tsx
@@ -1,68 +1,54 @@
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { useAuth } from '@/hooks/use-auth';
+import useFirestorePagination from '@/hooks/use-pagination';
+import { InvoiceData } from '@/lib/types';
+import NumberFlow from '@number-flow/react';
 
 export default function Recent() {
+	const { user, authLoading } = useAuth();
+	const { documents, loading } = useFirestorePagination({
+		userId: user?.uid || '',
+		pageSize: 10,
+	});
+
+	const data = documents as InvoiceData[];
+	if (authLoading || loading) return null;
 	return (
 		<Card>
 			<CardHeader>
 				<CardTitle>Recent Sales</CardTitle>
 			</CardHeader>
 			<CardContent className='grid gap-8'>
-				<div className='flex items-center gap-4'>
-					<Avatar className='hidden h-9 w-9 sm:flex'>
-						<AvatarImage src='/avatars/01.png' alt='Avatar' />
-						<AvatarFallback>OM</AvatarFallback>
-					</Avatar>
-					<div className='grid gap-1'>
-						<p className='text-sm font-medium leading-none'>Olivia Martin</p>
-						<p className='text-sm text-muted-foreground'>olivia.martin@email.com</p>
+				{data.map((doc, index) => (
+					<div className='flex items-center gap-4' key={index}>
+						<Avatar className='hidden h-9 w-9 sm:flex'>
+							<AvatarFallback className='bg-muted text-muted-foreground'>
+								{doc.billedTo.name
+									.split(' ')
+									.slice(0, 2)
+									.map((word) => word.charAt(0).toUpperCase())
+									.join('')}
+							</AvatarFallback>
+						</Avatar>
+						<div className='grid gap-1'>
+							<p className='text-sm font-medium leading-none'>{doc.billedTo.name}</p>
+							<p className='text-sm text-muted-foreground'>
+								{doc.createdAt?.toDate().toLocaleString('en-US', {
+									year: 'numeric',
+									month: 'short',
+									day: 'numeric',
+									hour: '2-digit',
+									minute: '2-digit',
+									hour12: true,
+								})}
+							</p>
+						</div>
+						<div className='ml-auto font-medium'>
+							<NumberFlow value={doc.total} format={{ style: 'currency', currency: 'LKR' }} />
+						</div>
 					</div>
-					<div className='ml-auto font-medium'>+$1,999.00</div>
-				</div>
-				<div className='flex items-center gap-4'>
-					<Avatar className='hidden h-9 w-9 sm:flex'>
-						<AvatarImage src='/avatars/02.png' alt='Avatar' />
-						<AvatarFallback>JL</AvatarFallback>
-					</Avatar>
-					<div className='grid gap-1'>
-						<p className='text-sm font-medium leading-none'>Jackson Lee</p>
-						<p className='text-sm text-muted-foreground'>jackson.lee@email.com</p>
-					</div>
-					<div className='ml-auto font-medium'>+$39.00</div>
-				</div>
-				<div className='flex items-center gap-4'>
-					<Avatar className='hidden h-9 w-9 sm:flex'>
-						<AvatarImage src='/avatars/03.png' alt='Avatar' />
-						<AvatarFallback>IN</AvatarFallback>
-					</Avatar>
-					<div className='grid gap-1'>
-						<p className='text-sm font-medium leading-none'>Isabella Nguyen</p>
-						<p className='text-sm text-muted-foreground'>isabella.nguyen@email.com</p>
-					</div>
-					<div className='ml-auto font-medium'>+$299.00</div>
-				</div>
-				<div className='flex items-center gap-4'>
-					<Avatar className='hidden h-9 w-9 sm:flex'>
-						<AvatarImage src='/avatars/04.png' alt='Avatar' />
-						<AvatarFallback>WK</AvatarFallback>
-					</Avatar>
-					<div className='grid gap-1'>
-						<p className='text-sm font-medium leading-none'>William Kim</p>
-						<p className='text-sm text-muted-foreground'>will@email.com</p>
-					</div>
-					<div className='ml-auto font-medium'>+$99.00</div>
-				</div>
-				<div className='flex items-center gap-4'>
-					<Avatar className='hidden h-9 w-9 sm:flex'>
-						<AvatarImage src='/avatars/05.png' alt='Avatar' />
-						<AvatarFallback>SD</AvatarFallback>
-					</Avatar>
-					<div className='grid gap-1'>
-						<p className='text-sm font-medium leading-none'>Sofia Davis</p>
-						<p className='text-sm text-muted-foreground'>sofia.davis@email.com</p>
-					</div>
-					<div className='ml-auto font-medium'>+$39.00</div>
-				</div>
+				))}
 			</CardContent>
 		</Card>
 	);
