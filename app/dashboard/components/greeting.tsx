4703b4578be13904e309a2636f784bc7e62f8457
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-07 02:19:33 +0530

    Added greeting message

--- app/dashboard/components/greeting.tsx
+++ app/dashboard/components/greeting.tsx
@@ -1,11 +1,11 @@
 'use client';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { useProfileStore } from '@/store/use-profile';
+import { useAuth } from '@/hooks/use-auth';
 import { HeartHandshakeIcon } from 'lucide-react';
 
 export default function Greeting() {
-	const { profile } = useProfileStore();
+	const { user } = useAuth();
 	const hour = new Date().getHours();
 	let greeting = '';
 
@@ -22,11 +22,12 @@ export default function Greeting() {
 	return (
 		<Card>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-				<CardTitle className='text-sm font-medium'>Hello {profile?.name},</CardTitle>
+				<CardTitle className='text-sm font-medium'>{greeting}</CardTitle>
 				<HeartHandshakeIcon className='h-4 w-4 text-muted-foreground' />
 			</CardHeader>
 			<CardContent>
-				<div className='text-2xl font-bold'>{greeting}</div>
+				<div className='bg-gradient-to-r from-violet-500 to-orange-400 bg-clip-text text-2xl font-bold text-transparent dark:to-orange-300'>{user?.displayName}</div>
+				<p className='text-sm text-muted-foreground'>Welcome to your hyperbooks dashboard.</p>
 			</CardContent>
 		</Card>
 	);
