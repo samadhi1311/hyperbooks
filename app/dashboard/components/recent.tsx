85ae29fe4617d5bbf2a619fb9a8434559b09efc9
nipunigamage <nipunikanishka255@gmail.com>
2025-02-07 02:18:43 +0530

    Added skeletons

--- app/dashboard/components/recent.tsx
+++ app/dashboard/components/recent.tsx
@@ -1,5 +1,6 @@
 import { Avatar, AvatarFallback } from '@/components/ui/avatar';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { Skeleton } from '@/components/ui/skeleton';
 import { useAuth } from '@/hooks/use-auth';
 import useFirestorePagination from '@/hooks/use-pagination';
 import { InvoiceData } from '@/lib/types';
@@ -20,35 +21,51 @@ export default function Recent() {
 				<CardTitle>Recent Sales</CardTitle>
 			</CardHeader>
 			<CardContent className='grid gap-8'>
-				{data.map((doc, index) => (
-					<div className='flex items-center gap-4' key={index}>
-						<Avatar className='hidden h-9 w-9 sm:flex'>
-							<AvatarFallback className='bg-muted text-muted-foreground'>
-								{doc.billedTo.name
-									.split(' ')
-									.slice(0, 2)
-									.map((word) => word.charAt(0).toUpperCase())
-									.join('')}
-							</AvatarFallback>
-						</Avatar>
-						<div className='grid gap-1'>
-							<p className='text-sm font-medium leading-none'>{doc.billedTo.name}</p>
-							<p className='text-sm text-muted-foreground'>
-								{doc.createdAt?.toDate().toLocaleString('en-US', {
-									year: 'numeric',
-									month: 'short',
-									day: 'numeric',
-									hour: '2-digit',
-									minute: '2-digit',
-									hour12: true,
-								})}
-							</p>
+				{documents.length > 0 &&
+					data.map((doc, index) => (
+						<div className='flex items-center gap-4' key={index}>
+							<Avatar className='hidden h-9 w-9 sm:flex'>
+								<AvatarFallback className='bg-muted text-muted-foreground'>
+									{doc.billedTo.name
+										.split(' ')
+										.slice(0, 2)
+										.map((word) => word.charAt(0).toUpperCase())
+										.join('')}
+								</AvatarFallback>
+							</Avatar>
+							<div className='grid gap-1'>
+								<p className='text-sm font-medium leading-none'>{doc.billedTo.name}</p>
+								<p className='text-sm text-muted-foreground'>
+									{doc.createdAt?.toDate().toLocaleString('en-US', {
+										year: 'numeric',
+										month: 'short',
+										day: 'numeric',
+										hour: '2-digit',
+										minute: '2-digit',
+										hour12: true,
+									})}
+								</p>
+							</div>
+							<div className='ml-auto font-medium'>
+								<NumberFlow value={doc.total} format={{ style: 'currency', currency: 'LKR' }} />
+							</div>
 						</div>
-						<div className='ml-auto font-medium'>
-							<NumberFlow value={doc.total} format={{ style: 'currency', currency: 'LKR' }} />
-						</div>
+					))}
+				{documents.length === 0 && (
+					<div className='mb-4 grid h-full gap-1'>
+						<p className='text-sm font-medium leading-none'>Your recent sales will appear here.</p>
 					</div>
-				))}
+				)}
+				{documents.length === 0 &&
+					Array.from({ length: 5 }).map((_, index) => (
+						<div className='flex items-center space-x-4' key={index}>
+							<Skeleton className='h-12 w-12 rounded-full' />
+							<div className='space-y-2'>
+								<Skeleton className='h-4 w-[250px]' />
+								<Skeleton className='h-4 w-[200px]' />
+							</div>
+						</div>
+					))}
 			</CardContent>
 		</Card>
 	);
