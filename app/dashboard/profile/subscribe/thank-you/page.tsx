a3cd0de56a18f2276f4663acadcf457aa46b108b
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-06 13:37:06 +0530

    Added checkout return page

---
+++ app/dashboard/profile/subscribe/thank-you/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function ThankYou() {
+	return (
+		<div className='flex h-full flex-col items-center justify-center'>
+			<h1 className='text-2xl font-bold'>Thank you for your payment!</h1>
+		</div>
+	);
+}
