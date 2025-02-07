1dd0f3b3894bf7134eb38b8829e4913979cfc0e7
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-07 10:03:27 +0530

    Added data pre fills

--- app/dashboard/profile/subscribe/components/payment.tsx
+++ app/dashboard/profile/subscribe/components/payment.tsx
@@ -4,10 +4,13 @@ import { initializePaddle, Paddle } from '@paddle/paddle-js';
 import { Button } from '@/components/ui/button';
 import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
+import { useAuth } from '@/hooks/use-auth';
+import { Section } from '@/components/ui/layout';
 
 export default function Payment() {
 	const [paddle, setPaddle] = useState<Paddle>();
 	const { theme } = useTheme();
+	const { user } = useAuth();
 
 	useEffect(() => {
 		initializePaddle({
@@ -17,21 +20,26 @@ export default function Payment() {
 	}, []);
 
 	const handleCheckout = () => {
-		if (!paddle) return alert('Paddle not initialized');
+		if (!paddle || !user?.email) return alert('Paddle not initialized');
 
 		paddle.Checkout.open({
 			items: [{ priceId: 'pri_01jkctkft82s247d8y3w6px2zj', quantity: 1 }],
+			customer: {
+				email: user.email,
+			},
 			settings: {
+				allowLogout: false,
 				displayMode: 'overlay',
 				theme: theme === 'dark' ? 'dark' : 'light',
+				variant: 'multi-page',
 				successUrl: 'http://localhost:3000/dashboard/profile/subscribe/thank-you',
 			},
 		});
 	};
 
 	return (
-		<>
+		<Section>
 			<Button onClick={handleCheckout}>Subscribe</Button>
-		</>
+		</Section>
 	);
 }
