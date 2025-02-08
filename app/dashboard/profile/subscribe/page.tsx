560b469283279085917b8fa50680f8339f454314
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-09 00:25:15 +0530

    Added Paddle Customer Portal

--- app/dashboard/profile/subscribe/page.tsx
+++ app/dashboard/profile/subscribe/page.tsx
@@ -1,14 +1,30 @@
 'use client';
 
 import { Section } from '@/components/ui/layout';
-import { useEffect } from 'react';
 import Payment from './components/payment';
+import { useUserStore } from '@/store/use-user';
+import { Button } from '@/components/ui/button';
 
 export default function Subscription() {
-	useEffect(() => {}, []);
+	const { userData } = useUserStore();
+
+	const fetchPortalLink = async () => {
+		if (userData?.customerId) {
+			const response = await fetch(`https://hyperbooks-api.hyperreal.cloud/customer-portal`, {
+				method: 'POST',
+				body: JSON.stringify({ customer_id: userData.customerId }),
+			});
+
+			const result = await response.json();
+			console.log(result);
+			window.open(result.url, '_blank');
+		}
+	};
+
 	return (
 		<Section>
 			<Payment />
+			<Button onClick={fetchPortalLink}>Manage Subscription</Button>
 		</Section>
 	);
 }
