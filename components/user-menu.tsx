f22b8c9bf5dae174a8515eaa9cc9e4268c8b68d5
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-09 00:23:53 +0530

    Added menu functionalities

--- components/user-menu.tsx
+++ components/user-menu.tsx
@@ -1,15 +1,17 @@
 'use client';
 
-import { BadgeCheck, Bell, ChevronsUpDown, CreditCard, LogOut, Sparkles } from 'lucide-react';
+import { BadgeCheck, ChevronsUpDown, CreditCard, LogOut, Sparkles } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from '@/components/ui/dropdown-menu';
 import { SidebarMenu, SidebarMenuButton, SidebarMenuItem, useSidebar } from '@/components/ui/sidebar';
 import { ModeToggle } from './theme-toggle';
 import { useAuth } from '@/hooks/use-auth';
+import { useRouter } from 'next/navigation';
 
 export function UserMenu() {
 	const { isMobile } = useSidebar();
-	const { user } = useAuth();
+	const { user, logout } = useAuth();
+	const router = useRouter();
 	if (!user) return null;
 	return (
 		<SidebarMenu>
@@ -17,8 +19,8 @@ export function UserMenu() {
 				<DropdownMenu>
 					<DropdownMenuTrigger asChild>
 						<SidebarMenuButton size='lg' className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'>
-							<Avatar className='h-8 w-8 rounded-full'>
-								<AvatarImage src={user.photoURL!} alt={user.displayName!} />
+							<Avatar className='flex size-8 items-center justify-center rounded-full border border-muted-foreground bg-muted'>
+								<AvatarImage className='size-6 object-contain' src={user.photoURL!} alt={user.displayName!} />
 								<AvatarFallback className='rounded-lg'>{user.displayName?.charAt(0)}</AvatarFallback>
 							</Avatar>
 							<div className='grid flex-1 text-left text-sm leading-tight'>
@@ -43,32 +45,27 @@ export function UserMenu() {
 						</DropdownMenuLabel>
 						<DropdownMenuSeparator />
 						<DropdownMenuGroup>
-							<DropdownMenuItem>
+							<DropdownMenuItem onClick={() => router.push('/account/upgrade')}>
 								<Sparkles />
 								Upgrade to Pro
 							</DropdownMenuItem>
 						</DropdownMenuGroup>
 						<DropdownMenuSeparator />
 						<DropdownMenuGroup>
-							<DropdownMenuItem>
+							<DropdownMenuItem onClick={() => router.push('/dashboard/account')}>
 								<BadgeCheck />
 								Account
 							</DropdownMenuItem>
-							<DropdownMenuItem>
+							<DropdownMenuItem onClick={() => router.push('/dashboard/account')}>
 								<CreditCard />
-								Billing
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<Bell />
-								Notifications
+								Manage Subscription
 							</DropdownMenuItem>
 						</DropdownMenuGroup>
 						<DropdownMenuSeparator />
 						<DropdownMenuItem>
-							<ModeToggle />
-							Toggle theme
+							<ModeToggle variant='secondary' />
 						</DropdownMenuItem>
-						<DropdownMenuItem>
+						<DropdownMenuItem onClick={logout}>
 							<LogOut />
 							Log out
 						</DropdownMenuItem>
