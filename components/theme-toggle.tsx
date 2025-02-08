ded6b8e0ce41c9a34c8276330f5997f19221672b
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-09 00:24:09 +0530

    Added toggle variants

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -4,7 +4,7 @@ import { useTheme } from 'next-themes';
 import { Button } from '@/components/ui/button';
 import { Moon, Sun } from 'lucide-react';
 
-export function ModeToggle() {
+export function ModeToggle({ variant = 'default' }: { variant?: 'default' | 'secondary' }) {
 	const { theme, setTheme } = useTheme();
 
 	function handleToggle() {
@@ -13,6 +13,18 @@ export function ModeToggle() {
 		} else {
 			setTheme('dark');
 		}
+	}
+
+	if (variant === 'secondary') {
+		return (
+			<span className='flex items-center gap-2' onClick={handleToggle}>
+				<span className='relative flex h-4 w-4 items-center justify-center'>
+					<Sun className='size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+					<Moon className='absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+				</span>
+				<span className='inline'>Toggle Theme</span>
+			</span>
+		);
 	}
 
 	return (
