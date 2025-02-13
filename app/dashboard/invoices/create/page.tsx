8216ba55a6d77c90e1a8db67ca75f5e70c591a07
nipunigamage <nipunikanishka255@gmail.com>
2025-02-13 13:56:43 +0530

    Added Single page for render Create Invoice

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -1,73 +1,17 @@
 'use client';
 
-import { PageWrapper } from '@/components/ui/layout';
-import templates, { TemplateKey } from '@/templates';
-import { useTemplateStore } from '@/store/use-templates';
-import { useInvoiceStore } from '@/store/use-invoice';
+import FormView from './components/form-view';
+import InvoiceView from './components/invoice-view';
 import Menu from '@/components/menu';
-import { Suspense, useEffect, useState } from 'react';
-import Loader from '@/components/ui/loader';
-import { useFirestore } from '@/hooks/use-firestore';
-import { ProfileData } from '@/lib/types';
-import { useAuth } from '@/hooks/use-auth';
+import { useViewStore } from '@/store/use-view';
 
 export default function CreateInvoice() {
-	const { user, authLoading } = useAuth();
-	const { getProfile } = useFirestore();
-	const { selectedTemplate } = useTemplateStore();
-
-	const [profile, setProfile] = useState<ProfileData | null>(null);
-
-	const { invoiceData, updateInvoiceData, updateBilledToData, updateItemData, addItem, removeItem } = useInvoiceStore();
-
-	const handleNestedInputChange = (path: string, value: string) => {
-		const keys = path.split('.');
-		if (keys[0] === 'billedTo') {
-			updateBilledToData({ [keys[1]]: value });
-		} else {
-			updateInvoiceData({ [keys[0]]: value });
-		}
-	};
-
-	const handleArrayChange = (path: string, index: number, value: any, field?: string) => {
-		if (path === 'items') {
-			if (index === -1) {
-				// Add new item
-				addItem();
-			} else {
-				// Update existing item
-				updateItemData(index, { [field || 'description']: value });
-			}
-		} else if (path === 'billedTo.address') {
-			const newAddress = invoiceData.billedTo.address ? [...invoiceData.billedTo.address] : [];
-			newAddress[index] = value;
-			updateBilledToData({ address: newAddress });
-		}
-	};
-
-	useEffect(() => {
-		if (user?.uid) {
-			const fetchProfile = async () => {
-				const profileData = await getProfile();
-				setProfile(profileData);
-			};
-			fetchProfile();
-		}
-	}, [authLoading]);
-
-	if (!selectedTemplate || !profile?.name) return <Loader />;
-
-	const SelectedTemplate = templates[selectedTemplate as TemplateKey].component;
+	const { view } = useViewStore();
 
 	return (
-		<PageWrapper>
-			<div className='my-16 grid items-center justify-center'>
-				<Menu />
-
-				<Suspense fallback={<Loader />}>
-					<SelectedTemplate profile={profile} data={invoiceData} removeItem={removeItem} onEdit={handleNestedInputChange} onArrayEdit={handleArrayChange} />
-				</Suspense>
-			</div>
-		</PageWrapper>
+		<div className='mx-auto flex w-full flex-col items-center justify-center'>
+			<Menu />
+			{view === 'form' ? <FormView /> : <InvoiceView />}
+		</div>
 	);
 }
