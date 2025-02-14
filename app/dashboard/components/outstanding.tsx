e11c183882f87822b20fe36fa2cfe5df6a715346
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-14 22:11:23 +0530

    Updated to fetch from Analytics Store

--- app/dashboard/components/outstanding.tsx
+++ app/dashboard/components/outstanding.tsx
@@ -2,19 +2,19 @@ import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { AlertTriangleIcon, TrendingDownIcon, TrendingUpIcon } from 'lucide-react';
 import NumberFlow from '@number-flow/react';
 import { useEffect, useState } from 'react';
-import { useUserStore } from '@/store/use-user';
+import { useAnalyticsStore } from '@/store/use-analytics';
 
 export default function Outstanding() {
-	const { userData } = useUserStore();
+	const { analytics } = useAnalyticsStore();
 	const [count, setCount] = useState(0);
 	const [amount, setAmount] = useState(0);
 
 	useEffect(() => {
 		setTimeout(() => {
-			setCount(userData?.totalOutstandingCount ?? 0);
-			setAmount(userData?.totalOutstandingAmount ?? 0);
+			setCount(analytics?.totalOutstandingCount ?? 0);
+			setAmount(analytics?.totalOutstandingAmount ?? 0);
 		}, 500);
-	}, [userData?.totalOutstandingCount, userData?.totalOutstandingAmount]);
+	}, [analytics?.totalOutstandingCount, analytics?.totalOutstandingAmount]);
 	return (
 		<Card className={count > 1 ? 'border-orange-300 dark:border-orange-300/30' : ''}>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
