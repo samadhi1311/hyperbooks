149b1528066761fdb2bdece001caa7534e6688a3
nipunigamage <nipunikanishka255@gmail.com>
2025-02-17 23:27:38 +0530

    Updated background effects

--- app/dashboard/components/outstanding.tsx
+++ app/dashboard/components/outstanding.tsx
@@ -3,6 +3,7 @@ import { AlertTriangleIcon, TrendingDownIcon, TrendingUpIcon } from 'lucide-reac
 import NumberFlow from '@number-flow/react';
 import { useEffect, useState } from 'react';
 import { useAnalyticsStore } from '@/store/use-analytics';
+import { cn } from '@/lib/utils';
 
 export default function Outstanding() {
 	const { analytics } = useAnalyticsStore();
@@ -16,7 +17,7 @@ export default function Outstanding() {
 		}, 500);
 	}, [analytics?.totalOutstandingCount, analytics?.totalOutstandingAmount]);
 	return (
-		<Card className={count > 1 ? 'border-orange-300 dark:border-orange-300/30' : ''}>
+		<Card className={cn('relative z-10 bg-background/60 shadow-xl backdrop-blur-sm', count > 1 ? 'border-orange-300 dark:border-orange-300/30' : '')}>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
 				<CardTitle className='text-sm font-medium'>Outstandings</CardTitle>
 				<AlertTriangleIcon className={count > 1 ? 'h-4 w-4 text-orange-500 dark:text-orange-300/50' : 'h-4 w-4 text-muted-foreground'} />
