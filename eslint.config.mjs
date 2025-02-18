078999bdbd714091b24346f78bba091788c38549
nipunigamage <nipunikanishka255@gmail.com>
2025-02-18 13:52:00 +0530

    Updated ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,7 @@ const eslintConfig = [
 			'@next/next/no-img-element': 'off',
 			'@typescript-eslint/no-explicit-any': 'off',
 			'react-hooks/exhaustive-deps': 'off',
+			'@typescript-eslint/no-unused-vars': 'warn',
 		},
 	},
 ];
