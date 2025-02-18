05239c2839348dd775a9d771ef3130c05f680541
nipunigamage <nipunikanishka255@gmail.com>
2025-02-18 13:49:44 +0530

    Added minor style changes

--- app/dashboard/components/outstanding.tsx
+++ app/dashboard/components/outstanding.tsx
@@ -17,7 +17,7 @@ export default function Outstanding() {
 		}, 500);
 	}, [analytics?.totalOutstandingCount, analytics?.totalOutstandingAmount]);
 	return (
-		<Card className={cn('relative z-10 bg-background/60 shadow-xl backdrop-blur-sm', count > 1 ? 'border-orange-300 dark:border-orange-300/30' : '')}>
+		<Card className={cn('relative z-10 bg-background/60 shadow-md backdrop-blur-sm', count > 1 ? 'border-orange-300 dark:border-orange-300/30' : '')}>
 			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
 				<CardTitle className='text-sm font-medium'>Outstandings</CardTitle>
 				<AlertTriangleIcon className={count > 1 ? 'h-4 w-4 text-orange-500 dark:text-orange-300/50' : 'h-4 w-4 text-muted-foreground'} />
