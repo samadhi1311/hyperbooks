856e85669212ba23f66cafe1b8791a6ddda4f32b
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-19 09:34:04 +0530

    Added usage and internet requirements

--- components/protected-route.tsx
+++ components/protected-route.tsx
@@ -1,13 +1,26 @@
 'use client';
 
-import { useEffect } from 'react';
+import { ReactNode, useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/hooks/use-auth';
 import Loader from './ui/loader';
+import { useOnlineStatus } from '@/hooks/use-online-status';
+import Offline from './offline';
+import { useFirestore } from '@/hooks/use-firestore';
+import { Plan } from '@/lib/types';
+import { toast } from '@/hooks/use-toast';
 
-export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+interface ProtectedRouteProps {
+	children: ReactNode;
+	requiredPlan?: Plan;
+}
+
+export const ProtectedRoute = ({ children, requiredPlan = 'starter' }: ProtectedRouteProps) => {
 	const { user, authLoading } = useAuth();
+	const { getUser } = useFirestore();
 	const router = useRouter();
+	const onlineStatus = useOnlineStatus();
+	const [accessAllowed, setAccessAllowed] = useState(requiredPlan === 'starter');
 
 	useEffect(() => {
 		if (!authLoading && !user) {
@@ -15,11 +28,35 @@ export const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
 		}
 	}, [user, authLoading, router]);
 
+	useEffect(() => {
+		const fetchUserData = async () => {
+			if (requiredPlan !== 'starter') {
+				const userData = await getUser();
+				if (userData?.plan === requiredPlan) {
+					setAccessAllowed(true);
+				} else {
+					toast({
+						title: 'Access Denied',
+						description: `You are not allowed to access this page. Please upgrade your plan to access this page.`,
+						variant: 'destructive',
+					});
+					router.replace('/pricing');
+				}
+			}
+		};
+
+		fetchUserData();
+	}, [requiredPlan, router]);
+
 	if (authLoading) {
 		return <Loader />;
 	}
 
-	if (!user) {
+	if (!onlineStatus) {
+		return <Offline />;
+	}
+
+	if (!accessAllowed) {
 		return null;
 	}
 
