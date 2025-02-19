26322e1e067c9b4575929ec82d9b4aed99d3e2e0
nipunigamage <nipunikanishka255@gmail.com>
2025-02-19 09:30:09 +0530

    Added constant data

---
+++ lib/constants.ts
@@ -0,0 +1,14 @@
+export const PLAN_LIMITS = {
+	free: {
+		invoicesPerMonth: 10,
+		billsPerMonth: 10,
+	},
+	pro: {
+		invoicesPerMonth: 100,
+		billsPerMonth: 100,
+	},
+	ultimate: {
+		invoicesPerMonth: Infinity,
+		billsPerMonth: Infinity,
+	},
+};
