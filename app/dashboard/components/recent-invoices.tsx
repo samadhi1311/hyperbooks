f18e2134960461c6253154d8767888f5fd6390f6
nipunigamage <nipunikanishka255@gmail.com>
2025-02-20 09:35:55 +0530

    Added minor style improvements

--- app/dashboard/components/recent-invoices.tsx
+++ app/dashboard/components/recent-invoices.tsx
@@ -43,8 +43,8 @@ export default function RecentInvoices() {
 										.join('')}
 								</AvatarFallback>
 							</Avatar>
-							<div className='grid gap-1'>
-								<p className='text-sm font-medium leading-none'>{doc.billedTo.name}</p>
+							<div className='grid gap-0.5'>
+								<p className='w-[200px] truncate pb-px text-sm font-medium leading-none'>{doc.billedTo.name}</p>
 								<p className='text-sm text-muted-foreground'>
 									{doc.createdAt?.toDate().toLocaleString('en-US', {
 										year: 'numeric',
@@ -62,8 +62,8 @@ export default function RecentInvoices() {
 						</div>
 					))
 				) : (
-					<div className='mb-4 flex w-full flex-col items-center justify-center'>
-						<p className='text-sm font-medium leading-none text-muted-foreground'>Your recent sales will appear here.</p>
+					<div className='mb-4'>
+						<p className='text-sm font-medium leading-none text-muted-foreground/50'>Your recent sales will appear here.</p>
 					</div>
 				)}
 			</CardContent>
