da38f2d5d5464e12b42dce039ea4a8a5d9cf9bad
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-20 09:39:13 +0530

    Fixed typo

--- app/dashboard/profile/subscribe/page.tsx
+++ app/dashboard/profile/subscribe/page.tsx
@@ -9,10 +9,10 @@ export default function Subscription() {
 	const { userData } = useUserStore();
 
 	const fetchPortalLink = async () => {
-		if (userData?.customerId) {
+		if (userData?.customer_id) {
 			const response = await fetch(`https://hyperbooks-api.hyperreal.cloud/customer-portal`, {
 				method: 'POST',
-				body: JSON.stringify({ customer_id: userData.customerId }),
+				body: JSON.stringify({ customer_id: userData.customer_id }),
 			});
 
 			const result = await response.json();
