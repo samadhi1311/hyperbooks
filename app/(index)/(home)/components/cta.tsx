a66a1f89625c76cc8c87650d042bfe3e46d80c61
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-23 21:54:29 +0530

    style: Replaced default button with IconButton component

--- app/(index)/(home)/components/cta.tsx
+++ app/(index)/(home)/components/cta.tsx
@@ -2,8 +2,8 @@
 
 import { Section } from '@/components/ui/layout';
 import { H2, P } from '@/components/ui/typography';
-import { Button } from '@/components/ui/button';
-import { LogInIcon } from 'lucide-react';
+import { IconButton } from '@/components/ui/icon-button';
+import { SquareDashedMousePointerIcon } from 'lucide-react';
 
 export default function CTA() {
 	return (
@@ -13,10 +13,9 @@ export default function CTA() {
 				<P className='max-w-2xl text-center text-muted-foreground'>
 					Take the stress out of invoicing with our simple yet powerful tools. Sign up today and make your billing as polished as your business!
 				</P>
-				<Button variant='secondary' className='mt-4 flex items-center gap-2'>
-					<LogInIcon />
-					Sign up
-				</Button>
+				<IconButton variant='secondary' className='mt-4' icon={<SquareDashedMousePointerIcon className='scale-x-[-1]' />}>
+					Get Started
+				</IconButton>
 			</div>
 			<div className='absolute bottom-0 -z-50 h-full w-full bg-[radial-gradient(ellipse_at_bottom,_var(--tw-gradient-stops))] from-foreground/10 via-transparent to-transparent dark:from-foreground/5 dark:via-transparent dark:to-transparent' />
 		</Section>
