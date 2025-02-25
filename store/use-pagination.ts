9ad1fe5e5abfb5f43f5ad9f2c29b56c01d839a85
nipunigamage <nipunikanishka255@gmail.com>
2025-02-25 23:22:33 +0530

    fix: Moved to session storage

--- store/use-pagination.ts
+++ store/use-pagination.ts
@@ -1,5 +1,6 @@
 import { DocumentData } from 'firebase/firestore';
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface PaginationStore {
 	documents: DocumentData[];
@@ -8,15 +9,34 @@ interface PaginationStore {
 	setCurrentPage: (page: number) => void;
 	clearAllInvoices: () => void;
 	loading: boolean;
-	setLoading: (loading: boolean) => void; // New setter for loading state
+	setLoading: (loading: boolean) => void;
 }
 
-export const usePaginationStore = create<PaginationStore>((set) => ({
-	documents: [],
-	setDocuments: (docs) => set({ documents: docs }),
-	currentPage: 1,
-	setCurrentPage: (page) => set({ currentPage: page }),
-	clearAllInvoices: () => set({ documents: [], currentPage: 1 }),
-	loading: false, // Default loading state
-	setLoading: (loading) => set({ loading }), // Set loading state
-}));
+export const usePaginationStore = create<PaginationStore>()(
+	persist(
+		(set) => ({
+			documents: [],
+			setDocuments: (docs) => set({ documents: docs }),
+			currentPage: 1,
+			setCurrentPage: (page) => set({ currentPage: page }),
+			clearAllInvoices: () => set({ documents: [], currentPage: 1 }),
+			loading: false,
+			setLoading: (loading) => set({ loading }),
+		}),
+		{
+			name: 'hyperbooks-invoice-pagination-storage',
+			storage: {
+				getItem: (key) => {
+					const value = sessionStorage.getItem(key);
+					return value ? JSON.parse(value) : null;
+				},
+				setItem: (key, value) => {
+					sessionStorage.setItem(key, JSON.stringify(value));
+				},
+				removeItem: (key) => {
+					sessionStorage.removeItem(key);
+				},
+			},
+		}
+	)
+);
