9147b79f22aea5757f51f3b657fb6182e8461bea
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-02-25 23:23:41 +0530

    refactor: Refactored code for consistant coding style

--- store/use-view.ts
+++ store/use-view.ts
@@ -1,14 +1,34 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type ViewState = {
 	view: 'form' | 'invoice';
 	setView: (view: 'form' | 'invoice') => void;
 };
 
-export const useViewStore = create<ViewState>((set) => ({
-	view: (typeof window !== 'undefined' && (localStorage.getItem('selected-view') as 'form' | 'invoice')) || 'invoice',
-	setView: (view) => {
-		localStorage.setItem('selected-view', view);
-		set({ view });
-	},
-}));
+export const useViewStore = create<ViewState>()(
+	persist(
+		(set) => ({
+			view: (typeof window !== 'undefined' && (localStorage.getItem('selected-view') as 'form' | 'invoice')) || 'invoice',
+			setView: (view) => {
+				localStorage.setItem('selected-view', view);
+				set({ view });
+			},
+		}),
+		{
+			name: 'hyperbooks-view-storage',
+			storage: {
+				getItem: (key) => {
+					const value = localStorage.getItem(key);
+					return value ? JSON.parse(value) : null;
+				},
+				setItem: (key, value) => {
+					localStorage.setItem(key, JSON.stringify(value));
+				},
+				removeItem: (key) => {
+					localStorage.removeItem(key);
+				},
+			},
+		}
+	)
+);
