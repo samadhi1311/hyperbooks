c45ac7fb67abe7d1ef6a7cb78c8d49f3129ac53b
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-03-01 23:55:44 +0530

    feat: Improved mobile responsiveness

--- app/dashboard/components/dashboard.tsx
+++ app/dashboard/components/dashboard.tsx
@@ -43,14 +43,14 @@ export default function Dashboard() {
 
 	return (
 		<div className='relative flex w-full flex-col overflow-clip'>
-			<section className='flex h-full flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8'>
-				<div className='grid gap-4 md:grid-cols-2 md:gap-8 xl:grid-cols-4'>
+			<section className='flex h-full flex-1 flex-col gap-2 p-1 md:gap-8 md:p-8'>
+				<div className='grid grid-cols-2 gap-2 md:gap-8 xl:grid-cols-4'>
 					<Greeting />
 					<Revenue />
 					<Outstanding />
 					<Invoices />
 				</div>
-				<div className='grid h-full grid-cols-1 grid-rows-1 gap-4 md:gap-8 xl:grid-cols-5 xl:grid-rows-2 xl:gap-y-8 2xl:grid-cols-3'>
+				<div className='grid h-full grid-cols-1 grid-rows-1 gap-2 md:gap-8 xl:grid-cols-5 xl:grid-rows-2 xl:gap-y-8 2xl:grid-cols-3'>
 					<div className='h-full xl:col-span-3 xl:row-span-2 2xl:col-span-2'>
 						<Chart />
 					</div>
