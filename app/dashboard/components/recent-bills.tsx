2b485ac171835c83ff0912fdf720e02d02333521
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-03-01 23:54:35 +0530

    feat: Added Memoization to improve performance

--- app/dashboard/components/recent-bills.tsx
+++ app/dashboard/components/recent-bills.tsx
@@ -6,14 +6,41 @@ import { useBillPaginationStore } from '@/store/use-bill-pagination';
 import { useUserStore } from '@/store/use-user';
 import { ListRestartIcon } from 'lucide-react';
 import { Timestamp } from 'firebase/firestore';
+import { useMemo } from 'react';
 
 export default function RecentBills() {
 	const { userData } = useUserStore();
 	const { documents, loading } = useBillPaginationStore();
 	const data = documents as BillData[];
 
+	const memoizedData = useMemo(
+		() =>
+			data.slice(0, 5).map((doc) => ({
+				...doc,
+				initials: doc.description
+					.split(' ')
+					.slice(0, 2)
+					.map((word) => word.charAt(0).toUpperCase())
+					.join(''),
+				formattedDate:
+					doc.createdAt && Timestamp.prototype.isPrototypeOf(doc.createdAt)
+						? doc.createdAt.toDate().toLocaleString('en-US', {
+								year: 'numeric',
+								month: 'short',
+								day: 'numeric',
+								hour: '2-digit',
+								minute: '2-digit',
+								hour12: true,
+						  })
+						: 'Date not available',
+			})),
+		[data]
+	);
+
+	if (!memoizedData) return null;
+
 	return (
-		<Card className='relative z-10 h-full bg-background/60 shadow-md backdrop-blur-sm'>
+		<Card className='relative z-10 h-full bg-background/60 shadow-none sm:shadow-md backdrop-blur-none sm:backdrop-blur-sm'>
 			<CardHeader>
 				<CardTitle className='flex items-center gap-3 text-base text-muted-foreground'>
 					<ListRestartIcon className='size-5' />
@@ -24,40 +51,23 @@ export default function RecentBills() {
 				{loading ? (
 					Array.from({ length: 5 }).map((_, index) => (
 						<div className='flex items-center space-x-4' key={index}>
-							<Skeleton className='h-9 w-9 rounded-full' />
+							<Skeleton className='size-8 rounded-full' />
 							<div className='space-y-2'>
-								<Skeleton className='h-4 w-[250px]' />
-								<Skeleton className='h-4 w-[200px]' />
+								<Skeleton className='h-3 w-[250px]' />
+								<Skeleton className='h-3 w-[200px]' />
 							</div>
 						</div>
 					))
-				) : data.length > 0 ? (
-					data.slice(0, 5).map((doc, index) => (
+				) : memoizedData.length > 0 ? (
+					memoizedData.slice(0, 5).map((doc, index) => (
 						<div className='flex items-start gap-4' key={index}>
 							<Avatar className='size-8'>
-								<AvatarFallback className='bg-muted text-muted-foreground'>
-									{doc.description
-										.split(' ')
-										.slice(0, 2)
-										.map((word) => word.charAt(0).toUpperCase())
-										.join('')}
-								</AvatarFallback>
+								<AvatarFallback className='bg-muted text-muted-foreground'>{doc.initials}</AvatarFallback>
 							</Avatar>
 							<div className='grid gap-0.5'>
 								<span className='inline-block font-medium sm:hidden'>{userData?.currency + ' ' + doc.amount.toFixed(2)}</span>
 								<p className='w-[200px] truncate pb-px text-sm font-medium leading-none'>{doc.description}</p>
-								<p className='text-xs text-muted-foreground'>
-									{doc.createdAt && Timestamp.prototype.isPrototypeOf(doc.createdAt)
-										? doc.createdAt.toDate().toLocaleString('en-US', {
-												year: 'numeric',
-												month: 'short',
-												day: 'numeric',
-												hour: '2-digit',
-												minute: '2-digit',
-												hour12: true,
-										  })
-										: 'Date not available'}
-								</p>
+								<p className='text-xs text-muted-foreground'>{doc.formattedDate}</p>
 							</div>
 							<span className='ml-auto hidden font-medium sm:inline-block'>{userData?.currency + ' ' + doc.amount.toFixed(2)}</span>
 						</div>
