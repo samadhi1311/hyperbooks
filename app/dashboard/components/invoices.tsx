764d1abe90dceffe9037a78f26f70fdeb56531d6
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-03-01 23:55:17 +0530

    feat: Improved performance and responsiveness on mobile devices

--- app/dashboard/components/invoices.tsx
+++ app/dashboard/components/invoices.tsx
@@ -16,16 +16,16 @@ export default function Invoices() {
 		}, 500);
 	}, [analytics?.totalInvoiceCount, analytics?.totalOutstandingCount]);
 	return (
-		<Card className='relative z-10 bg-background/60 shadow-md backdrop-blur-sm'>
-			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-				<CardTitle className='text-sm font-medium'>Completed Invoices</CardTitle>
-				<ScrollTextIcon className='h-4 w-4 text-muted-foreground' />
+		<Card className='relative z-10 bg-background/60 shadow-none sm:shadow-md backdrop-blur-none sm:backdrop-blur-sm'>
+			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-0 md:pb-2'>
+				<CardTitle className='text-xs font-medium md:text-sm'>Completed Invoices</CardTitle>
+				<ScrollTextIcon className='size-3 text-muted-foreground md:size-4' />
 			</CardHeader>
 			<CardContent>
-				<NumberFlow className='text-2xl font-bold' value={count - outstanding} />
-				<span className='flex items-center gap-2 text-sm text-muted-foreground'>
-					Total Invoices: <NumberFlow className='text-sm text-muted-foreground' value={count} />
-					{outstanding > 1 ? <TrendingDownIcon className='h-4 w-4 text-muted-foreground' /> : <TrendingUpIcon className='h-4 w-4 text-muted-foreground' />}
+				<NumberFlow className='text-base font-bold md:text-2xl' value={count - outstanding} />
+				<span className='flex items-center gap-2 text-xs text-muted-foreground md:text-sm'>
+					Total Invoices: <p className='text-sm text-muted-foreground'>{count}</p>
+					{outstanding > 1 ? <TrendingDownIcon className='size-3 md:size-4 text-muted-foreground' /> : <TrendingUpIcon className='size-3 md:size-4 text-muted-foreground' />}
 				</span>
 			</CardContent>
 		</Card>
