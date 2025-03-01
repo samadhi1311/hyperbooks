5a44f94fe83a9ad387ac2f69a02645521843130d
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-03-01 23:55:10 +0530

    feat: Improved performance and responsiveness on mobile devices

--- app/dashboard/components/outstanding.tsx
+++ app/dashboard/components/outstanding.tsx
@@ -17,16 +17,16 @@ export default function Outstanding() {
 		}, 500);
 	}, [analytics?.totalOutstandingCount, analytics?.totalOutstandingAmount]);
 	return (
-		<Card className={cn('relative z-10 bg-background/60 shadow-md backdrop-blur-sm', count > 1 ? 'border-orange-300 dark:border-orange-300/30' : '')}>
-			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-				<CardTitle className='text-sm font-medium'>Outstandings</CardTitle>
-				<AlertTriangleIcon className={count > 1 ? 'h-4 w-4 text-orange-500 dark:text-orange-300/50' : 'h-4 w-4 text-muted-foreground'} />
+		<Card className={cn('relative z-10 bg-background/60 shadow-none sm:shadow-md backdrop-blur-none sm:backdrop-blur-sm', count > 1 ? 'border-orange-300 dark:border-orange-300/30' : '')}>
+			<CardHeader className='flex flex-row items-center justify-between space-y-0 pb-0 md:pb-2'>
+				<CardTitle className='text-xs font-medium md:text-sm'>Outstandings</CardTitle>
+				<AlertTriangleIcon className={count > 1 ? 'size-3 md:size-4 text-orange-500 dark:text-orange-300/50' : 'size-3 md:size-4 text-muted-foreground'} />
 			</CardHeader>
 			<CardContent>
-				<NumberFlow className='text-2xl font-bold' format={{ notation: 'standard', style: 'currency', currency: 'LKR' }} value={amount} />
+				<NumberFlow className='text-base font-bold md:text-2xl' format={{ notation: 'standard', style: 'currency', currency: 'LKR' }} value={amount} />
 				<span className='flex items-center gap-2 text-sm text-muted-foreground'>
-					Outstanding Invoices: <NumberFlow className='text-sm text-muted-foreground' value={count} />
-					{count > 1 ? <TrendingDownIcon className='h-4 w-4 text-muted-foreground' /> : <TrendingUpIcon className='h-4 w-4 text-muted-foreground' />}
+					Outstanding Invoices: <p className='text-xs text-muted-foreground md:text-sm'>{count}</p>
+					{count > 1 ? <TrendingDownIcon className='size-3 md:size-4 text-muted-foreground' /> : <TrendingUpIcon className='size-3 md:size-4 text-muted-foreground' />}
 				</span>
 			</CardContent>
 		</Card>
