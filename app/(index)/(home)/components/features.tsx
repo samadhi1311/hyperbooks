271a1233cceec9773c390313fb6b8822b1e9f9ba
nipunigamage <nipunikanishka255@gmail.com>
2025-03-02 22:56:13 +0530

    feat: Updated for improved mobile responsiveness

--- app/(index)/(home)/components/features.tsx
+++ app/(index)/(home)/components/features.tsx
@@ -3,14 +3,14 @@
 import { Section } from '@/components/ui/layout';
 import { H2, P } from '@/components/ui/typography';
 import { cn } from '@/lib/utils';
-import { PaletteIcon, PaintbrushIcon, CloudUploadIcon, Share2Icon, MonitorSmartphoneIcon, SparklesIcon } from 'lucide-react';
+import { CloudUploadIcon, SaveAllIcon, ChartNoAxesCombinedIcon, EarthIcon, ScrollTextIcon, BrainCircuitIcon } from 'lucide-react';
 
 export default function Features() {
 	const Feature = ({ title, description, icon, index }: { title: string; description: string; icon: React.ReactNode; index: number }) => {
 		return (
 			<div
 				className={cn(
-					'flex flex-col lg:border-r py-16 relative group/feature dark:border-neutral-800',
+					'flex flex-col lg:border-r py-4 md:py-8 lg:py-16 relative group/feature dark:border-neutral-800',
 					(index === 0 || index === 3) && 'lg:border-l dark:border-neutral-800',
 					index < 3 && 'lg:border-b dark:border-neutral-800'
 				)}>
@@ -20,8 +20,8 @@ export default function Features() {
 				{index >= 3 && (
 					<div className='pointer-events-none absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-200/50 to-transparent opacity-0 transition duration-200 group-hover/feature:opacity-100 dark:from-neutral-800/30' />
 				)}
-				<div className='relative z-10 mb-4 px-8 text-muted-foreground'>{icon}</div>
-				<div className='relative z-10 mb-4 px-8 text-lg font-medium text-foreground'>
+				<div className='relative z-10 mb-3 px-8 text-muted-foreground md:mb-4'>{icon}</div>
+				<div className='relative z-10 mb-3 px-8 text-lg font-medium text-foreground md:mb-4'>
 					<div className='absolute inset-y-0 left-0 h-6 w-1 origin-center rounded-br-full rounded-tr-full bg-neutral-300 transition-all duration-200 group-hover/feature:h-8 group-hover/feature:bg-violet-500 dark:bg-neutral-700' />
 					<h3 className='font-display inline-block text-neutral-800 transition duration-200 group-hover/feature:translate-x-2 dark:text-neutral-100'>{title}</h3>
 				</div>
@@ -32,46 +32,46 @@ export default function Features() {
 
 	const features = [
 		{
-			title: 'Beautifully designed templates',
-			description: 'Choose from dozens of professionally designed templates',
-			icon: <PaletteIcon />,
+			title: 'Track income & expenses in just a few taps',
+			description: 'Log your earnings and expenses fast, intuitive, and accurately. Stay on top of your finances without the hassle.',
+			icon: <SaveAllIcon />,
 		},
 		{
-			title: 'Make it yours',
-			description: 'Build your own templates from blocks to match your unique style and business needs.',
-			icon: <PaintbrushIcon />,
+			title: 'See your cash flow at a glance',
+			description: 'See the bigger picture at a glance with real-time daily and monthly summaries. Know exactly where your money is going and make smarter financial decisions.',
+			icon: <ChartNoAxesCombinedIcon />,
 		},
 		{
-			title: 'Save to Cloud',
-			description: 'Save invoices securely to the cloud and access them anytime, from anywhere.',
+			title: 'Keep data always safe & backed up',
+			description: 'Your financial data is protected with top-tier security and automatic backups. No worries, no data loss—just peace of mind.',
 			icon: <CloudUploadIcon />,
 		},
 		{
-			title: 'Export and Share',
-			description: 'Generate and download invoices in high-quality PDF format with a single click.',
-			icon: <Share2Icon />,
+			title: 'Access From Anywhere',
+			description: 'Whether you&apos;re at the office, home, or on the move, access your financial records anytime with our cloud-based platform. Stay in control, wherever you are.',
+			icon: <EarthIcon />,
 		},
 		{
-			title: 'Multi-Platform Access',
-			description: 'Access and edit your invoices across devices – desktop, tablet, or mobile.',
-			icon: <MonitorSmartphoneIcon />,
+			title: 'Professional Invoicing Made Simple',
+			description: 'Create stunning, customizable invoices in seconds. Generate detailed PDF reports with ease and impress clients with a seamless billing experience.',
+			icon: <ScrollTextIcon />,
 		},
 		{
-			title: 'Get Started for Free',
-			description: 'Create and export invoices for free. No credit card required.',
-			icon: <SparklesIcon />,
+			title: 'AI-Powered insights and suggestions',
+			description: 'Unlock smarter insights with AI-driven analytics and suggestions. Detect patterns, optimize cash flow, and get proactive recommendations tailored to your business.',
+			icon: <BrainCircuitIcon />,
 		},
 	];
 
 	return (
 		<Section>
-			<div className='flex scroll-my-16 flex-col items-center gap-4 py-8' id='features'>
-				<H2>Everything You Need to Create Professional Invoices.</H2>
-				<P className='max-w-2xl text-center text-muted-foreground'>
-					Say goodbye to boring invoices! With customizable templates, cloud storage, and more, we make invoicing fun and effortless—just the way it should be.
+			<div className='flex scroll-my-16 flex-col items-center gap-4 py-8 text-center lg:py-16' id='features'>
+				<H2>Stay organized, stay in control.</H2>
+				<P className='max-w-2xl text-muted-foreground'>
+					Track finances effortlessly, access records anywhere, and keep data secure. Create invoices easily and gain AI-driven insights for smarter decisions.
 				</P>
 			</div>
-			<div className='relative z-10 mx-auto grid grid-cols-1 py-16 md:grid-cols-2 lg:grid-cols-3'>
+			<div className='relative z-10 mx-auto grid grid-cols-1 py-4 md:grid-cols-2 md:py-8 lg:grid-cols-3 lg:py-16'>
 				{features.map((feature, index) => (
 					<Feature key={feature.title} {...feature} index={index} />
 				))}
