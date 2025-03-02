f66bc8de77c5b05972aa8a92dcae5407a8b13964
nipunigamage <nipunikanishka255@gmail.com>
2025-03-02 22:56:55 +0530

    feat: Added gradient background effects

--- app/(index)/(home)/page.tsx
+++ app/(index)/(home)/page.tsx
@@ -8,11 +8,21 @@ import CTA from './components/cta';
 
 export default function Home() {
 	return (
-		<PageWrapper>
-			<Hero />
-			<Features />
-			<Pricing />
-			<CTA />
-		</PageWrapper>
+		<div className='relative w-screen overflow-hidden'>
+			<PageWrapper>
+				<Hero />
+				<div
+					className='hyperbooks-gradient-blob-1 pointer-events-none absolute left-0 -z-50 hidden aspect-square w-2/3 max-w-5xl -translate-x-1/2 opacity-20 dark:opacity-5 md:block'
+					aria-hidden
+				/>
+				<Features />
+				<div
+					className='hyperbooks-gradient-blob-2 pointer-events-none absolute right-0 -z-50 hidden aspect-square w-2/3 max-w-5xl translate-x-1/2 translate-y-1/2 opacity-30 dark:opacity-15 md:block'
+					aria-hidden
+				/>
+				<Pricing />
+				<CTA />
+			</PageWrapper>
+		</div>
 	);
 }
