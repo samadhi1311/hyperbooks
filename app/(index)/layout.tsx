a0befc83bf4d2782f28321a7970662083a8cc7e8
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-03-05 16:41:41 +0530

    refactor: Removed leftover metadata

--- app/(index)/layout.tsx
+++ app/(index)/layout.tsx
@@ -1,58 +1,5 @@
-import type { Metadata } from 'next';
 import Navigation from '@/components/navigation';
 import Footer from '@/components/footer';
-
-export const dynamic = 'force-static';
-
-export const metadata: Metadata = {
-	title: 'hyperbooks. | Your Invoicing, Simplified.',
-	description: 'Create, manage, and export professional invoices in seconds—anytime, anywhere.',
-	keywords: [
-		'hyperbooks',
-		'invoice generator',
-		'online invoice generator',
-		'Sri Lanka',
-		'Bookkeeping',
-		'Taxation',
-		'Accounting',
-		'Accounting Software',
-		'Accounting Software Sri Lanka',
-		'Accounting Software in Sri Lanka',
-		'Accounting Software for Sri Lanka',
-	],
-	authors: [
-		{
-			name: 'Samadhi Gunasinghe',
-			url: 'https://hyperreal.cloud',
-		},
-		{
-			name: 'Nipuni Gamage',
-			url: 'https://hyperreal.cloud',
-		},
-	],
-	robots: {
-		index: true,
-		follow: true,
-	},
-	openGraph: {
-		title: 'hyperbooks. | Your Invoicing, Simplified.',
-		description: 'Create, manage, and export professional invoices in seconds—anytime, anywhere.',
-		type: 'website',
-		url: 'https://hyperbooks.hyperreal.cloud/',
-		images: [
-			{
-				url: 'https://hyperbooks.hyperreal.cloud/meta-image.jpg',
-			},
-		],
-	},
-	twitter: {
-		title: 'hyperbooks. | Your Invoicing, Simplified.',
-		description: 'Create, manage, and export professional invoices in seconds—anytime, anywhere.',
-		card: 'summary_large_image',
-		site: '@hyperrealhq',
-		images: ['https://hyperbooks.hyperreal.cloud/meta-image.jpg'],
-	},
-};
 
 export default function IndexLayout({ children }: Readonly<{ children: React.ReactNode }>) {
 	return (
