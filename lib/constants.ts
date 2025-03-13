f519926b4583ef67d48fc832ac4e85117ab5cff5
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-03-13 13:09:47 +0530

    chore: Added plan perks

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,4 +1,4 @@
-export const PLAN_LIMITS = {
+const PLAN_LIMITS = {
 	starter: {
 		invoicesPerMonth: 5,
 		billsPerMonth: 5,
@@ -27,3 +27,35 @@ export const PLAN_LIMITS = {
 		ai: true,
 	},
 };
+
+const starterPerks = [
+	'Total of 10 records per month',
+	'Select any free templates from the library',
+	'20 invoice exports per month',
+	'Daily analytics for the last 30 days.',
+	'Monthly analytics for all time.',
+	'hyperbooks branding.',
+	'No AI Powered Insights.',
+];
+
+const proPerks = [
+	'Total of 50 records per month',
+	'Unlimited invoice exports',
+	'Daily analytics for the last 90 days',
+	'Monthly analytics for all time',
+	'AI-Powered Insights',
+	'Custom-made invoice template',
+	'No hyperreal banding',
+];
+
+const ultimatePerks = [
+	'Total of 1000 records per month',
+	'Unlimited invoice exports',
+	'Daily analytics for the last 365 days',
+	'Monthly analytics for all time',
+	'AI-Powered Insights',
+	'Custom-made invoice template',
+	'No hyperreal banding',
+];
+
+export { PLAN_LIMITS, starterPerks, proPerks, ultimatePerks };
