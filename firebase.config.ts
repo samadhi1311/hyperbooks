5bdece2076b0272319d4795b1122ca0ade3ec3d8
nipunigamage <nipunikanishka255@gmail.com>
2025-03-19 23:10:29 +0530

    chore: Removed Google Analytics

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,7 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { getAnalytics, isSupported } from 'firebase/analytics';
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,9 +17,5 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
-let analytics;
-isSupported().then((yes) => {
-	if (yes) analytics = getAnalytics(app);
-});
 
-export { app, auth, db, storage, analytics };
+export { app, auth, db, storage };
