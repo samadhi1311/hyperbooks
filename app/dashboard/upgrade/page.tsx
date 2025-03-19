0de5aa0ba1fb65ef6a284324c4a230c6d0418a93
nipunigamage <nipunikanishka255@gmail.com>
2025-03-19 23:00:14 +0530

    chore: Updated pricing perks

--- app/dashboard/upgrade/page.tsx
+++ app/dashboard/upgrade/page.tsx
@@ -5,7 +5,6 @@ import { H1, P, H3 } from '@/components/ui/typography';
 import { TextShimmer } from '@/components/ui/text-shimmer';
 import {} from '@/components/ui/typography';
 import { Badge } from '@/components/ui/badge';
-import FlickeringGrid from '@/components/ui/flickering-grid';
 import { MagicCard } from '@/components/ui/magic-card';
 import { CheckCircle2Icon, CircleXIcon, SparkleIcon, SparklesIcon } from 'lucide-react';
 import { initializePaddle, Paddle } from '@paddle/paddle-js';
@@ -13,6 +12,7 @@ import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 import { useAuth } from '@/hooks/use-auth';
 import { IconButton } from '@/components/ui/icon-button';
+import { starterPerks, proPerks, ultimatePerks } from '@/lib/constants';
 
 export default function Upgrade() {
 	const [paddle, setPaddle] = useState<Paddle>();
@@ -53,7 +53,7 @@ export default function Upgrade() {
 
 	return (
 		<PageWrapper>
-			<Section className='mx-auto flex h-full w-full max-w-screen-lg flex-col items-center justify-center'>
+			<Section className='mx-auto flex h-full w-full flex-col items-center justify-center'>
 				<div className='space-y-4 text-center'>
 					<H1>
 						<TextShimmer className='py-1.5' duration={3}>
@@ -67,87 +67,80 @@ export default function Upgrade() {
 					<MagicCard className='cursor-pointer flex-col items-center justify-center' gradientColor={theme === 'dark' ? '#202020' : '#dfdfdf'}>
 						<div className='p-8'>
 							<Badge>Starter</Badge>
-							<H3 className='mb-2 mt-6 text-2xl md:text-3xl'>Free</H3>
-							<P variant='sm'>Free forever. No credit card required.</P>
-							<P className='pt-8'>Essentials to get started,</P>
-							<ul className='ml-4 space-y-2 py-4'>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>10 invoices per month</P>
-								</li>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>Access to all templates</P>
-								</li>
-								<li className='flex items-center gap-2'>
-									<CircleXIcon className='size-5' />
-									<P>&apos;hyperbooks.&apos; branding</P>
-								</li>
+
+							<H3 className='mt-4 flex items-baseline'>
+								<span className='text-2xl font-semibold tracking-tight md:text-3xl'>$0</span>
+								<span className='ml-1 text-base font-semibold md:text-xl'>/month</span>
+							</H3>
+							<p className='mt-6 text-sm text-muted-foreground md:text-base'>Free forever. No credit card required.</p>
+
+							<ul role='list' className='mt-6 space-y-6 border-t border-border pt-6'>
+								<span className='text-sm font-semibold text-muted-foreground md:text-base'>What&apos;s included?</span>
+
+								{starterPerks.map((perk, index) => (
+									<li className='flex' key={index}>
+										<div className='inline-flex items-center'>
+											{index < starterPerks.length - 2 ? <CheckCircle2Icon className='size-4 md:size-6' /> : <CircleXIcon className='size-4 text-muted-foreground md:size-6' />}
+										</div>
+										<p className='ml-3 text-sm md:text-base'>{perk}</p>
+									</li>
+								))}
 							</ul>
 						</div>
 					</MagicCard>
 					<MagicCard className='flex h-full cursor-pointer flex-col items-center justify-between' gradientColor={theme === 'dark' ? '#202020' : '#dfdfdf'}>
 						<div className='h-full p-8'>
 							<Badge>Pro</Badge>
-							<H3 className='mb-2 mt-6 text-2xl md:text-3xl'>
-								LKR 700 <span className='text-base'>per month</span>
+
+							<H3 className='mt-4 flex items-baseline'>
+								<span className='text-2xl font-semibold tracking-tight md:text-3xl'>$3.99</span>
+								<span className='ml-1 text-base font-semibold md:text-xl'>/month</span>
 							</H3>
-							<P variant='sm'>For small and medium businesses.</P>
-							<P className='pt-8'>Everything from Starter, plus</P>
-							<ul className='ml-4 space-y-2 py-4'>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>100 records/month</P>
-								</li>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>No &apos;hyperbooks.&apos; branding</P>
-								</li>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>1 custom template</P>
-								</li>
+							<p className='mt-6 text-sm text-muted-foreground md:text-base'>The essentials for freelancers.</p>
+
+							<ul role='list' className='mt-6 space-y-6 border-t border-muted-foreground pt-6 dark:border-muted-foreground'>
+								<span className='text-sm font-semibold text-muted-foreground md:text-base'>What&apos;s included?</span>
+
+								{proPerks.map((perk, index) => (
+									<li className='flex' key={index}>
+										<div className='inline-flex items-center'>
+											<CheckCircle2Icon className='size-4 md:size-6' />
+										</div>
+										<P className='ml-3 text-sm md:text-base'>{perk}</P>
+									</li>
+								))}
 							</ul>
-							<div className='flex flex-col items-center justify-end pt-4'>
+							<div className='flex flex-col items-center justify-center pt-8'>
 								<IconButton icon={<SparkleIcon />} onClick={() => handleCheckout({ priceId: 'pri_01jnn26q26hsz815nwa7fdzke1', plan: 'pro' })}>
 									Upgrade to Pro
 								</IconButton>
 							</div>
 						</div>
-						<FlickeringGrid
-							className='pointer-events-none absolute inset-0 -z-10 overflow-hidden [mask-image:radial-gradient(circle_at_50%_50%,white_0,transparent_80%)]'
-							squareSize={4}
-							gridGap={8}
-							color='#8db8a4'
-							maxOpacity={0.3}
-							flickerChance={0.2}
-							width={1000}
-							height={1000}
-						/>
 					</MagicCard>
 					<MagicCard className='cursor-pointer flex-col items-center justify-between' gradientColor={theme === 'dark' ? '#202020' : '#dfdfdf'}>
 						<div className='p-8'>
 							<Badge>Ultimate</Badge>
-							<H3 className='mb-2 mt-6 text-2xl md:text-3xl'>
-								LKR 2400 <span className='text-base'>per month</span>
+
+							<H3 className='mt-4 flex items-baseline'>
+								<span className='text-2xl font-semibold tracking-tight md:text-3xl'>$9.99</span>
+								<span className='ml-1 text-base font-semibold md:text-xl'>/month</span>
 							</H3>
-							<P variant='sm'>Ultimate control over your transactions.</P>
-							<P className='pt-8'>Everything from Pro, plus</P>
-							<ul className='ml-4 space-y-2 py-4'>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>Unlimited records/month</P>
-								</li>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>3 custom templates</P>
-								</li>
-								<li className='flex items-center gap-2'>
-									<CheckCircle2Icon className='size-5 text-emerald-500' />
-									<P>Premium 24/7 support</P>
-								</li>
+							<p className='mt-6 text-sm text-muted-foreground md:text-base'>The Ultimate control over your transactions.</p>
+
+							<ul role='list' className='mt-6 space-y-6 border-t border-border pt-6'>
+								<span className='text-sm font-semibold text-muted-foreground md:text-base'>What&apos;s included?</span>
+
+								{ultimatePerks.map((perk, index) => (
+									<li className='flex' key={index}>
+										<div className='inline-flex items-center'>
+											<CheckCircle2Icon className='size-4 md:size-6' />
+										</div>
+										<P className='ml-3 text-sm md:text-base'>{perk}</P>
+									</li>
+								))}
 							</ul>
-							<div className='flex items-center justify-center pt-4'>
+
+							<div className='flex items-center justify-center pt-8'>
 								<IconButton icon={<SparklesIcon />} onClick={() => handleCheckout({ priceId: 'pri_01jnn28fcbdyda07k4k0rm1yx2', plan: 'ultimate' })}>
 									Upgrade to Ultimate
 								</IconButton>
@@ -158,7 +151,7 @@ export default function Upgrade() {
 
 				<div>
 					<P variant='sm' className='text-muted-foreground'>
-						* Prices are in Sri Lankan Rupees (LKR).
+						* Prices are in United States Dollar (USD).
 					</P>
 				</div>
 			</Section>
