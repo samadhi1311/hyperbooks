2bbc5bc2dae6b631f28cb32e6bb258bf54f6700b
samadhi1311 <samadhilakmina.sl@gmail.com>
2025-03-20 16:19:25 +0530

    chore: Added recaptcha protection

--- firebase.config.ts
+++ firebase.config.ts
@@ -2,6 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
+import { initializeAppCheck, ReCaptchaV3Provider } from 'firebase/app-check';
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -18,4 +19,9 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-export { app, auth, db, storage };
+const appCheck = initializeAppCheck(app, {
+	provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_FIREBASE_RECAPTCHA_SITE_KEY!),
+	isTokenAutoRefreshEnabled: true,
+});
+
+export { app, auth, db, storage, appCheck };
