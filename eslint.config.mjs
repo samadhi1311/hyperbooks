376c2423b224fe533ea4eec02a271449bd709f15
samadhi1311 <samadhilakmina.sl@gmail.com>
2026-02-05 22:56:56 +0530

    feat: upgrade packages to latest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,19 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-	baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-	...compat.extends('next/core-web-vitals', 'next/typescript'),
-	{
-		rules: {
-			'@next/next/no-img-element': 'off',
-			'@typescript-eslint/no-explicit-any': 'off',
-			'@react-hooks/exhaustive-deps': 'off',
-		},
-	},
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+    rules: {
+        '@next/next/no-img-element': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@react-hooks/exhaustive-deps': 'off',
+    },
+}, {
+    ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
