f28ca53572640b79702e1c3aa7bfe48c10733000
nipunigamage <nipunikanishka255@gmail.com>
2026-02-10 23:35:38 +0530

    chore: add price format utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function fmtPrice(price: number | string) {
+  const num = Number(price);
+  return num.toLocaleString('en-US', {
+    style: 'decimal',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+    useGrouping: true
+  });
+}
