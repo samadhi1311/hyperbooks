afde55614bb0983f08f5c26bbbfeee6a55c81378
nipunigamage <nipunikanishka255@gmail.com>
2026-02-10 23:36:01 +0530

    chore: make the header fixed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -64,7 +64,7 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 			<SidebarProvider className='relative min-h-svh overflow-hidden'>
 				<AppSidebar variant='floating' collapsible='offcanvas' />
 				<SidebarInset>
-					<header className='flex h-16 shrink-0 items-center gap-2'>
+					<header className='fixed lg:static top-0 z-50 flex w-full h-10 shrink-0 items-center gap-2 border-b bg-sidebar/50 backdrop-blur-md'>
 						<div className='flex items-center gap-2 px-4'>
 							<SidebarTrigger className='-ml-1' />
 							<Separator orientation='vertical' className='mr-2 h-4' />
@@ -74,26 +74,26 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
 										<span key={index} className={path[index] === '' ? 'hidden' : 'flex items-center justify-center gap-2'}>
 											<BreadcrumbItem>
 												<BreadcrumbLink asChild>
-													<Link href={`${path.slice(0, index + 1).join('/')}`} className='capitalize'>
+													<Link href={`${path.slice(0, index + 1).join('/')}`} className='capitalize text-xs'>
 														{item}
 													</Link>
 												</BreadcrumbLink>
 											</BreadcrumbItem>
-											<BreadcrumbSeparator className={index === path.length - 1 ? 'hidden' : 'hidden md:block'} style={path.length < 1 ? { opacity: 0 } : { opacity: 1 }} />
+											<BreadcrumbSeparator className={index === path.length - 1 ? 'hidden' : 'block'} style={path.length < 1 ? { opacity: 0 } : { opacity: 1 }} />
 										</span>
 									))}
 								</BreadcrumbList>
 							</Breadcrumb>
 						</div>
 					</header>
-					<div className='max-w-[100svw]'>
+					<div className='pt-10 lg:pt-4 max-w-[100svw]'>
 						<Installer />
 						{children}
 					</div>
 				</SidebarInset>
-				<div className='hyperbooks-gradient-blob-1 pointer-events-none absolute right-0 top-0 aspect-square w-1/2 opacity-100 dark:opacity-10 md:left-0 md:w-1/3 md:opacity-20' aria-hidden />
+				<div className='hyperbooks-gradient-blob-1 pointer-events-none absolute right-0 top-0 aspect-square w-1/2 opacity-100 dark:opacity-10 md:left-0 md:w-1/3 md:opacity-20 z-0' aria-hidden />
 				<div
-					className='hyperbooks-gradient-blob-2 pointer-events-none absolute -bottom-1/3 -left-1/2 aspect-square w-1/3 rounded-full opacity-100 dark:opacity-20 md:-right-1/4 md:left-auto md:w-1/2 md:opacity-30'
+					className='hyperbooks-gradient-blob-2 pointer-events-none absolute -bottom-1/3 -left-1/2 aspect-square w-1/3 rounded-full opacity-100 dark:opacity-20 md:-right-1/4 md:left-auto md:w-1/2 md:opacity-20 z-0'
 					aria-hidden
 				/>
 			</SidebarProvider>
